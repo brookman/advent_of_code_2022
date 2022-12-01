2f6a307eff125cbc777d470376ac6dfb2fbc2041
Benjamin Bachmann <benjamin.bachmann@zuehlke.com>
2022-12-01 06:23:15 +0100

    Solution to Dec 1st

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
-use crate::common::read_numbers;
+use itertools::Itertools;
+
+use crate::common::read_strings;
 
 mod common;
 
 fn main() {
     println!("\nDecember 1st, 2022");
     dec_01_one("data/in_01_1.txt");
-    dec_01_two("data/in_01_2.txt");
+    dec_01_two("data/in_01_1.txt");
 }
 
 fn dec_01_one(filename: &str) {
     println!("--- Part One ---");
 
-    let numbers = read_numbers(filename);
-    for number in numbers {
-        println!("{:?}", number);
-    }
+    let sums = get_sum_of_blocks(filename);
+    let result = sums.iter().max().unwrap();
+
+    println!("{:?}", result);
 }
 
 fn dec_01_two(filename: &str) {
     println!("--- Part Two ---");
 
-    let numbers = read_numbers(filename);
-    for number in numbers {
-        println!("{:?}", number);
+    let sums = get_sum_of_blocks(filename);
+    let result: u32 = sums.into_iter().sorted_by(|a, b| b.cmp(a)).take(3).sum();
+
+    println!("{:?}", result);
+}
+
+fn get_sum_of_blocks(filename: &str) -> Vec<u32> {
+    let strings = read_strings(filename);
+
+    let mut sums: Vec<u32> = vec![0];
+    for string in strings {
+        if string.is_empty() {
+            sums.push(0);
+        } else {
+            let index = sums.len() - 1;
+            sums[index] += string.parse::<u32>().unwrap();
+        }
     }
+    sums
 }
